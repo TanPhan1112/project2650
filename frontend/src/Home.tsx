23240a34232be081f7f23e1eeff72267fdf5eccc
hoangnamngx882 <hoangnamngx882@gmail.com>
2021-07-30 06:59:11 +0000

    Bootstrap collapse chat button

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,93 +4,110 @@ function Home() {
     return (
         <html lang="en">
             <body>
-                    <main className="container">
-                        <hr></hr>
-                        <div className="row mb-2">
-                            <div className="col-md-6">
-                                <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                    <div className="col p-4 d-flex flex-column position-static">
-                                        <h3 className="mb-0">Featured post</h3>
-                                        <div className="mb-1 text-muted">Date</div>
-                                        <p className="card-text mb-auto">Add post content here</p>
-                                    </div>
-                                    <div className="col-auto d-none d-lg-block">
-                                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                    <div className="col p-4 d-flex flex-column position-static">
-                                        <h3 className="mb-0">Featured post</h3>
-                                        <div className="mb-1 text-muted">Date</div>
-                                        <p className="mb-auto">Add post content here.</p>
-                                    </div>
-                                    <div className="col-auto d-none d-lg-block">
-                                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="row mb-2">
-                            <div className="col-md-6">
-                                <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                    <div className="col p-4 d-flex flex-column position-static">
-                                        <h3 className="mb-0">Featured post</h3>
-                                        <div className="mb-1 text-muted">Date</div>
-                                        <p className="card-text mb-auto">Add post content here</p>
-                                    </div>
-                                    <div className="col-auto d-none d-lg-block">
-                                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                    <div className="col p-4 d-flex flex-column position-static">
-                                        <h3 className="mb-0">Featured post</h3>
-                                        <div className="mb-1 text-muted">Date</div>
-                                        <p className="mb-auto">Add post content here.</p>
-                                    </div>
-                                    <div className="col-auto d-none d-lg-block">
-                                        <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </main>
-                    
+                <main className="container">
                     <hr></hr>
-                    <footer className="footer mt-auto py-3 bg-light">
-                        <div className="container">
-                            <h3 className="text-center">Join Our Newsletter</h3>
-                            <h6 className="text-center">Sign up to all the latest offers, news and tips!</h6>
-
-
-                            <form method="get">
-                                <div className="row justify-content-center">
-                                    <input type="email" className="col-4" name="email" id="email" placeholder="Email address" />
-                                    <input className="col-2 signup btn btn-primary" type="submit" value="Sign me up!" />
-                                    <input className="col-1 signup btn btn-primary" type="reset" />
+                    <div className="row mb-2">
+                        <div className="col-md-6">
+                            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                                <div className="col p-4 d-flex flex-column position-static">
+                                    <h3 className="mb-0">Featured post</h3>
+                                    <div className="mb-1 text-muted">Date</div>
+                                    <p className="card-text mb-auto">Add post content here</p>
                                 </div>
-                            </form>
+                                <div className="col-auto d-none d-lg-block">
+                                    <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
-
-                            <figure className="text-center">
-                                <blockquote className="blockquote">
-                                    <p>Copyright &copy; 2021 pctech.com</p>
-                                </blockquote>
-                                <figcaption className="blockquote-footer">
-                                    Created by <cite title="authors">Phan, Duc Minh Tan and Nguyen, Hoang Nam</cite>
-                                </figcaption>
-                            </figure>
+                                </div>
+                            </div>
                         </div>
-                    </footer>
+                        <div className="col-md-6">
+                            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                                <div className="col p-4 d-flex flex-column position-static">
+                                    <h3 className="mb-0">Featured post</h3>
+                                    <div className="mb-1 text-muted">Date</div>
+                                    <p className="mb-auto">Add post content here.</p>
+                                </div>
+                                <div className="col-auto d-none d-lg-block">
+                                    <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row mb-2">
+                        <div className="col-md-6">
+                            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                                <div className="col p-4 d-flex flex-column position-static">
+                                    <h3 className="mb-0">Featured post</h3>
+                                    <div className="mb-1 text-muted">Date</div>
+                                    <p className="card-text mb-auto">Add post content here</p>
+                                </div>
+                                <div className="col-auto d-none d-lg-block">
+                                    <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                                <div className="col p-4 d-flex flex-column position-static">
+                                    <h3 className="mb-0">Featured post</h3>
+                                    <div className="mb-1 text-muted">Date</div>
+                                    <p className="mb-auto">Add post content here.</p>
+                                </div>
+                                <div className="col-auto d-none d-lg-block">
+                                    <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+
+                <hr></hr>
+
+                <p className="position-absolute bottom-20 end-0">
+                    <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-dots" viewBox="0 0 16 16">
+                            <path d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                            <path d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z"/>
+                        </svg>
+                    </button>
+                </p>
+                    
+                <div className="collapse" id="collapseExample">
+                    <div className="card card-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
+                </div>
+                
+
+                <footer className="footer mt-auto py-3 bg-light">
+                    <div className="container">
+                        <h3 className="text-center">Join Our Newsletter</h3>
+                        <h6 className="text-center">Sign up to all the latest offers, news and tips!</h6>
+
+
+                        <form method="get">
+                            <div className="row justify-content-center">
+                                <input type="email" className="col-4" name="email" id="email" placeholder="Email address" />
+                                <input className="col-2 signup btn btn-primary" type="submit" value="Sign me up!" />
+                                <input className="col-1 signup btn btn-primary" type="reset" />
+                            </div>
+                        </form>
+
+
+                        <figure className="text-center">
+                            <blockquote className="blockquote">
+                                <p>Copyright &copy; 2021 pctech.com</p>
+                            </blockquote>
+                            <figcaption className="blockquote-footer">
+                                Created by <cite title="authors">Phan, Duc Minh Tan and Nguyen, Hoang Nam</cite>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </footer>
             </body>
         </html>
     );
