382976e25d671cc0da6583111c70dad9d9d26ed5
Duc Minh Tan Phan <minhtan111293@gmail.com>
2021-08-04 10:21:01 +0000

    Automated Testing

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -11,24 +11,24 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  // load translation files from /public/locales
-  // learn more: https://github.com/i18next/i18next-http-backend
-  .use(HttpApi)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    detection: { order: ['path'], lookupFromPathIndex: 0 },
-    fallbackLng: 'en',
-    debug: true,
+    // load translation files from /public/locales
+    // learn more: https://github.com/i18next/i18next-http-backend
+    .use(HttpApi)
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        detection: { order: ['path'], lookupFromPathIndex: 0 },
+        fallbackLng: 'en',
+        debug: true,
 
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-  });
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+    });
 
 export default i18n;
