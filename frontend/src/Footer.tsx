43a5f5e2d0e86ec24168af5b10d19f2e5aacb821
hoangnamngx882 <hoangnamngx882@gmail.com>
2021-08-06 04:42:35 +0000

    Added chat button

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -10,6 +10,11 @@ function Footer() {
                 <div className="container">
                     <h3 className="text-center">{t('footer.footerTitle')}</h3>
                     <h6 className="text-center">{t('footer.footerHeading')}</h6>
+                    <div className="d-flex justify-content-center">
+                        <button type="button" className="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#liveChat">
+                            Chat support
+                        </button>
+                    </div>
 
                     <form method="get">
                         <div className="row justify-content-center">
@@ -18,7 +23,7 @@ function Footer() {
                             <button className="col-1 signup btn btn-primary" type="reset">{t('footer.reset')}</button>
                         </div>
                     </form>
-
+                    
                     <figure className="text-center">
                         <blockquote className="blockquote">
                             <p>{t('footer.copyright')} &copy; 2021 pctech.com</p>
@@ -27,6 +32,42 @@ function Footer() {
                             {t('footer.createdBy')} <cite title="authors">Phan, Duc Minh Tan and Nguyen, Hoang Nam</cite>
                         </figcaption>
                     </figure>
+                </div>
+
+                <div className="modal fade" id="liveChat" tabIndex={-1} aria-labelledby="liveChatLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable ">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="liveChatLabel">Live chat</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="chatArea">
+                                    <ul className="messages list-group">
+                                        <li className="list-group-item">
+                                            <div className="row">
+                                                <div className="col-2 mx-auto">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-file-person mt-2" viewBox="0 0 16 16">
+                                                        <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z" />
+                                                        <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                                    </svg>
+                                                </div>
+                                                <div className="col">
+                                                    Welcome to our chat support, leave a message and we will be with you soon!
+                                    </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Enter your message.." aria-label="messageInput" aria-describedby="button-addon2" />
+                                    <button type="submit" className="btn btn-outline-primary" id="button-addon2">Send</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </div>
